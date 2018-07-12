d48a0f93fccd314777d7c2eb6b9344c3dd0c84c5
Jason <guyi@kingsoft.com>
2018-07-12 13:07:52 +0800

    add requester

---
+++ spiderman/contrib/requesters/base.py
@@ -0,0 +1,9 @@
+
+
+
+
+class BaseRequester(object):
+
+    def add_requests(self): raise NotImplementedError('add_requests must be implemented')
+
+    def get_requests(self): raise NotImplementedError('add_requests must be implemented')
