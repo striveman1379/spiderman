97de738167cf63534629354a6498aa234970a67b
parnny <parnny@parnnydeMBP.lan>
2018-07-13 16:57:37 +0800

    erge codes

--- examples/imdb/setup.py
+++ examples/imdb/setup.py
@@ -1,5 +1,5 @@
 import pymysql
-import examples.spider_redis.spider_redis.settings.backends as backends
+import examples.imdb.imdb.settings.backends as backends
 
 settings = backends.BACKENDS['mysql']
 host = settings.get('HOST')
@@ -8,14 +8,13 @@ username = settings.get('USER')
 password = settings.get('PASSWD')
 database = settings.get('DBNAME')
 tablename = 'testtable'
+primary_key = 'rating'
 
 SqlMoveItem = {
-    'name': 'VARCHAR(255)',
-    'info': 'VARCHAR(255)',
+    'movie': 'VARCHAR(255)',
+    'url': 'VARCHAR(255)',
     'rating': 'INT',
-    'num': 'INT',
-    'quote': 'VARCHAR(255)',
-    'img_url': 'VARCHAR(255)',
+    'spider': 'VARCHAR(255)',
 }
 
 if __name__ == '__main__':
@@ -34,13 +33,11 @@ if __name__ == '__main__':
     connect_cursor.execute("create database %s default character set utf8mb4 collate utf8mb4_unicode_ci" % database)
     connect_cursor.execute("use %s" % database)
 
-    db_name = tablename
-    db_primary_key = 'name'
 
     # create table
     cmd_create_format = 'create table %s(%s,PRIMARY KEY (%s))'
     cmd_create_body = []
     for (k,t) in SqlMoveItem.items():
         cmd_create_body.append('%s %s' % (k, t))
-    cmd_create = cmd_create_format % (db_name, ','.join(cmd_create_body), db_primary_key)
+    cmd_create = cmd_create_format % (tablename, ','.join(cmd_create_body), primary_key)
     connect_cursor.execute(cmd_create)
