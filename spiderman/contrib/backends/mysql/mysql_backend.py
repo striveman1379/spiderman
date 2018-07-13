87a24cb7fa7590a7f5e3f0c3fa10a7cf0c467fd4
parnny <parnny@parnnydeMBP.lan>
2018-07-13 15:49:45 +0800

    add mysql backend

--- spiderman/contrib/backends/mysql/mysql_backend.py
+++ spiderman/contrib/backends/mysql/mysql_backend.py
@@ -1,90 +1,86 @@
 from spiderman.contrib.backends.backend import BaseBackend
-from scrapy.utils.misc import load_object
+import scrapy
+import pymysql
 
 MYSQL_HOST = 'localhost'
+MYSQL_PORT = '3306'
 MYSQL_DBNAME = 'database1'
 MYSQL_USER = 'root'
 MYSQL_PASSWD = '123456'
 MYSQL_TBNAME = 'table1'
 
-class MySqlBackend(BaseBackend):
 
-    def create_database(self):
+class MySqlBackend(BaseBackend):
+    connection = None
+    connect_cursor = None
+
+    def __init__(self, settings):
+        self.host = settings.get('HOST')
+        self.port = settings.get('PORT')
+        self.username = settings.get('USER')
+        self.password = settings.get('PASSWD')
+        self.database = settings.get('DBNAME')
+
+    def start(self):
+        if self.is_running():
+            return
+
+        super(MySqlBackend,self).start()
+
+        self.connection = pymysql.connect(
+            user=self.username,
+            password=self.password,
+            host=self.host,
+            port=self.port,
+            charset='utf8'
+        )
+        self.connect_cursor = self.connection.cursor()
+        self.connect_cursor.execute("use %s" % self.database)
+
+    def stop(self, reason):
+        if self.is_valid():
+            self.connect_cursor.close()
+            self.connection.commit()
+            self.connection.close()
+
+        self.connect_cursor = None
+        self.connection = None
+
+    def is_valid(self):
+        if self.connection is None:
+            return False
+        if self.connect_cursor is None:
+            return False
+        return True
+
+    def insert(self,tablename, item):
+        if not self.is_valid(): return
+
+        # insert item
+        cmd_format = "insert into %s(%s) value(%s)"
+        cmd_title = []
+        cmd_value = []
+
+        for (k,v) in item.items():
+            cmd_title.append(str(k))
+            if isinstance(v, str):
+                cmd_value.append("'{0}'".format(v))
+            else:
+                cmd_value.append(str(v))
+
+        cmd_insert = cmd_format % (tablename, ','.join(cmd_title), ','.join(cmd_value))
+        self.execute_command(cmd_insert)
+
+    def delete(self):
         pass
 
-    pass
+    def update(self):
+        pass
 
+    def query(self):
+        pass
 
-
-
-
-import scrapy
-import pymysql
-
-class MovieItem(scrapy.Item):
-    # 电影名字
-    name = scrapy.Field()
-    # 电影信息
-    info = scrapy.Field()
-    # 评分
-    rating = scrapy.Field()
-    # 评论人数
-    num = scrapy.Field()
-    # 经典语句
-    quote = scrapy.Field()
-    # 电影图片
-    img_url = scrapy.Field()
-
-
-
-'''
-create database douban DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-use douban;
-CREATE TABLE doubanmovie (
-    name VARCHAR(100) NOT NULL, # 电影名字
-    info VARCHAR(150), # 电影信息
-    rating VARCHAR(10), # 评分
-    num VARCHAR(10), # 评论人数
-    quote VARCHAR(100), # 经典语句
-    img_url VARCHAR(100), # 电影图片
-) 
-'''
-
-if __name__ == '__main__':
-    connect = pymysql.connect(
-        user='root',
-        password='123456',
-        host='127.0.01',
-        port=3306,
-        db='MYSQL',
-        charset='utf8'
-    )
-
-    connect_cursor = connect.cursor()
-
-    command = 'drop database if exists %s' % MYSQL_DATABASE_NAME
-    connect_cursor.execute(command)
-
-    item = MovieItem(name='name',info='info',rating=1,num=20,quote='quote',img_url='url')
-    format = 'insert into'
-    tabletitle = 'doubanmovie' + '('
-    tablevalue = 'value ('
-    for key in item.fields:
-        tabletitle += (key + ',')
-        tablevalue += (str(item[key]) + ',')
-
-    tabletitle = tabletitle[:-1]+')'
-    tablevalue = tablevalue[:-1]+')'
-
-        # """insert into doubanmovie(name, info, rating, num ,quote, img_url)
-        #                 value (%s, %s, %s, %s, %s, %s)""",
-        # (item['name'],
-        #  item['info'],
-        #  item['rating'],
-        #  item['num'],
-        #  item['quote'],
-        #  item['img_url']))
-    print(format)
-    print(tabletitle)
-    print(tablevalue)
-    pass
+    def execute_command(self, cmd):
+        if not self.is_valid(): return
+        self.connect_cursor.execute(cmd)
+        self.connection.commit()
