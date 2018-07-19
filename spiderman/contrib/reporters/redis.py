ca2e4cf737c3dc0fcd253461ed12535e501bce80
parnny <parnny@parnnydeMBP.lan>
2018-07-20 04:32:24 +0800

    temp

--- spiderman/contrib/reporters/redis.py
+++ spiderman/contrib/reporters/redis.py
@@ -30,10 +30,13 @@ class RedisReporter(BackendReporter):
 
     def on_download_exception(self, request, exception, spider):
         key = self._reporter_prefix + request.url
+        print(key)
+        print(spider.id)
+        print(str(traceback.format_exc()))
         self._backend.execute_command("HMSET", key,
                                       'spider_id', spider.id,
                                       'status', 'on_download_exception',
-                                      'exception', str(traceback.format_exc())
+                                      #'exception', str(traceback.format_exc())
                                       )
 
     def on_spider_exception(self, response, exception, spider):
